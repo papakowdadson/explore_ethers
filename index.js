81de9f4efbb45ce13a2f25fc6ed50ae5c7ae94b4
Odafe Aror <odafe41@gmail.com>
2023-08-21 15:23:16 +0000

    wip

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import {ethers } from 'ethers'
-import {PRIVATE_KEY, ganacheProvider} from './config.js'
+import {PRIVATE_KEY, PRIVATE_KEY_2, ganacheProvider} from './config.js'
+import Ganache from "ganache-core"
 
 const {utils, providers, parseEther, BrowserProvider, Wallet } = ethers
+
 
 const provider = new BrowserProvider(ganacheProvider)
 
 const wallet = new Wallet(PRIVATE_KEY, provider);
-// console.log(wallet);
 
-const wall2 =  Wallet.fromPhrase("plate lawn minor crouch bubble evidence palace fringe bamboo laptop dutch ice")
-console.log(wall2);
+const wall2 = new Wallet(PRIVATE_KEY_2, provider)
 
-wallet.signTransaction({
+// wallet.signTransaction({
+//     value: parseEther("1.0"),
+//     to: wall2.address,
+//     gasLimit: 21000,
+// }).then((tx) => {
+//     console.log('tx: ', tx);
+// })
+
+const server = Ganache.server()
+wallet.sendTransaction({
     value: parseEther("1.0"),
     to: wall2.address,
     gasLimit: 21000,
-}).then((tx) => {
+}, ).then((tx) => {
     console.log('tx: ', tx);
 })
+// Start the server
+server.listen(8545, (err, blockchain) => {
+    if (err) {
+        console.error('Error starting server:', err);
+    } else {
+        console.log('Ganache server listening on port 8545');
+        // You can now interact with the blockchain programmatically
+        // For example, you can use web3.js or ethers.js to connect and interact
+    }
+});
+
